8ce82384940f0a033e194c9bbb1bc87ee8c21637
James T. Harding <59349643+empowermint@users.noreply.github.com>
2022-03-12 18:03:40 +0000

    Add basic component files

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export class SearchBar extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+
+  render() {
+    return (
+      <div className="SearchBar">
+        <input placeholder="Enter A song, album, or artist" />
+        <button className="SearchButton">Search</button>
+      </div>
+    )
+  }
+}
---
+++ src/Components/TrackList/TrackList.js
@@ -0,0 +1,13 @@
+import './TrackList.css';
+
+import React from "react";
+
+export class TrackList extends React.Component {
+  render() {
+    return (
+      <div class="TrackList">
+      {/* TODO: Add a map method that renders a set of Track components */}
+      </div>
+    )
+  }
+}
